a36884a73acae9743f273869c45955c84a23a6ca
rafagurila11@gmail.com <rafagurila11@gmail.com>
2025-09-01 23:17:02 -0300

    add `/api/v1/status` endpoint

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return <h1>Conhecendo a Vercel</h1>;
+}
+
+function Teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("teste 2");
+}
+
+export default Home;
